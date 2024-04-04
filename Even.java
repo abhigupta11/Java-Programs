8479a132ffca1139bbda7d92c9eedb6c583d1bdf
abhigupta11 <abhishekgupta3268@gmail.com>
2024-04-04 11:53:40 +0530

    second program

---
+++ Even.java
@@ -0,0 +1,12 @@
+class Even
+{
+
+   public static void main(String[] args)
+   {
+        System.out.print("It is my first program");
+System.out.print("It is my second program");
+System.out.print("It is my third program");
+
+}
+
+} 
